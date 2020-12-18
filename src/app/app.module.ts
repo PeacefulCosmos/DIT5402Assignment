658bdba62a6a8f56953b901a01b50297ef910c3b
Roy Chan <cosmosroy0207@gmail.com>
2020-12-19 02:46:22 +0800

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { MaterialModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform browser/animations';
+// import { MaterialModule } from '@angular/material';
+// import { BrowserAnimationsModule } from '@angular/platform browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
